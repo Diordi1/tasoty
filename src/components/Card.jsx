c4ac383dee5654081371b30ef06adb66505d8499
wasteapp111@gmail.com <Diordi1>
2025-06-03 01:33:07 +0530

    readme-updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,22 @@
 import styles from './card.module.css';
-
+import { useState } from 'react';
+import DetailedCard from './DetailedCard';
 function Card(props) {
     console.log(props)
+     const [showDetails, setShowDetails] = useState(false);
+    
+    const toggleDetails = (e) => {
+        e.preventDefault();
+
+        setShowDetails(!showDetails);
+    };
     return ( 
         <div className={styles.card}>
             {/* "image": "https://cdn.dummyjson.com/recipe-images/1.webp", */}
             <div style={{display:"flex",justifyContent:"center",alignItems:"center",position:"relative",height:"125px",marginTop:"10px"}}>
 
             <img src={props.data1.image} width={160} height={140} style={{objectFit:"cover",display:"block"}}alt="img"></img>
-            <div className={styles.button}>i</div>
+            <div className={styles.button} onClick={toggleDetails}>i</div>
             </div>
             <p style={{fontWeight:"bold",}}>{props.data1.name}</p>
             {/* <p>Ingrediants : Tomato Sauce,Olive Oil</p> */}
@@ -18,6 +26,15 @@ function Card(props) {
             <p style={{fontWeight:"bold"}}>4.5 <img src="image.png" style={{width:"13px",height:"13px"}}></img></p>
             <p style={{fontWeight:"normal"}}>{props.data1.tags[props.data1.tags.length-1]}</p>
             </div>
+            <p style={{color:"white",backgroundColor:"purple",padding:2,textAlign:"center",borderRadius:"4px"}} onClick={toggleDetails}>Detailed Card</p>
+            {showDetails && (
+                <div className={styles.modalOverlay} onClick={toggleDetails}>
+                    <div className={styles.modalContent} onClick={(e) => e.preventDefault()}>
+                        <button className={styles.closeButton} onClick={toggleDetails}>×</button>
+                        <DetailedCard recipe={props.data1} />
+                    </div>
+                </div>
+            )}
         </div>
      );
 }
